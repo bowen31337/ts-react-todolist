042a7cb492e7cb44b15b9c5b85da9ca82d16119f
Bowen Li <bowen31337@gmail.com>
2024-05-27 20:06:12 +1000

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/ts-react-todolist/',
   plugins: [react()],
   test: {
     globals: true,
